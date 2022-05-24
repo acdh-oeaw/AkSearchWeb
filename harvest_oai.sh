62283d794bbf349ba2cf0f2107bcb91c856b7a17
ctot-nondef <christoph.hoffmann@oeaw.ac.at>
2022-05-24 15:23:41 +0200

    allowing for set specification in harvest_oai.sh

--- harvest_oai.sh
+++ harvest_oai.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 STATE=`ps ax | grep harvest_oai.php | wc -l`
-echo "--------------------------------"
+echo "---------------------------------"
+SET=$1
 date
 if [ "$STATE" == "1" ] ; then
-    php -f /var/www/vufind/harvest/harvest_oai.php
+    php -f /var/www/vufind/harvest/harvest_oai.php $SET
 else
     echo "harvest_oai.php is already running"
 fi
