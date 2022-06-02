a7ce042e0bb73fd2ef80b2eb9f866e97cbb09d1b
Mateusz Żółtak <zozlak@zozlak.org>
2022-06-02 15:25:51 +0200

    health_check_and_harvest.sh: perform harvesting in the background

--- health_check_and_harvest.sh
+++ health_check_and_harvest.sh
@@ -5,13 +5,13 @@ RET=$?
 HARVEST_PERIOD=360
 if [ "`find /var/www/local/harvest/alma/last_harvest.tx -maxdepth 0 -mmin -$HARVEST_PERIOD`" == "" ] ; then
     touch /var/www/local/harvest/alma/last_harvest.txt
-    /var/www/vufind/harvest/harvest_oai.sh
+    nohup /var/www/vufind/harvest/harvest_oai.sh >> /var/www/vufind/harvest/harvest_oai.log 2>&1
 fi
 
 HARVEST_PERIOD=1440
 if [ "`find /var/www/local/harvest/harvest_degruyter.json -maxdepth 0 -mmin -$HARVEST_PERIOD`" == "" ] ; then
     touch /var/www/local/harvest/harvest_degruyter.json
-    /var/www/vufind/harvest/harvest_degruyter.sh /var/www/local/harvest/harvest_degruyter.json $DEGRUYTER_PSWD
+    nohup /var/www/vufind/harvest/harvest_degruyter.sh /var/www/local/harvest/harvest_degruyter.json $DEGRUYTER_PSWD >> /var/www/vufind/harvest/harvest_degruyter.log 2>&1
 fi
 
 exit $RET
