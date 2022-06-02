799bb062ec497ea94223d329fb544cb7f5efe55c
Mateusz Żółtak <zozlak@zozlak.org>
2022-06-02 15:30:12 +0200

    restore dir separate filename test as it works also when files do not exist

--- health_check_and_harvest.sh
+++ health_check_and_harvest.sh
@@ -3,13 +3,13 @@ curl http://127.0.0.1/vufind/ > /dev/null 2>&1
 RET=$?
 
 HARVEST_PERIOD=360
-if [ "`find /var/www/local/harvest/alma/last_harvest.tx -maxdepth 0 -mmin -$HARVEST_PERIOD`" == "" ] ; then
+if [ "`find /var/www/local/harvest/alma -maxdepth 1 -name last_harvest.txt -mmin -$HARVEST_PERIOD`" == "" ] ; then
     touch /var/www/local/harvest/alma/last_harvest.txt
     nohup /var/www/vufind/harvest/harvest_oai.sh >> /var/www/vufind/harvest/harvest_oai.log 2>&1
 fi
 
 HARVEST_PERIOD=1440
-if [ "`find /var/www/local/harvest/harvest_degruyter.json -maxdepth 0 -mmin -$HARVEST_PERIOD`" == "" ] ; then
+if [ "`find /var/www/local/harvest -maxdepth 1 -name harvest_degruyter.json -mmin -$HARVEST_PERIOD`" == "" ] ; then
     touch /var/www/local/harvest/harvest_degruyter.json
     nohup /var/www/vufind/harvest/harvest_degruyter.sh /var/www/local/harvest/harvest_degruyter.json $DEGRUYTER_PSWD >> /var/www/vufind/harvest/harvest_degruyter.log 2>&1
 fi
