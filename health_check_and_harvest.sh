248aeb253340fea362dc2320cd8ec26186e27f8b
Mateusz Żółtak <zozlak@zozlak.org>
2022-06-02 14:40:13 +0200

    health_check_and_harvest.sh: touch before run to avoid concurrent runs

--- health_check_and_harvest.sh
+++ health_check_and_harvest.sh
@@ -4,14 +4,14 @@ RET=$?
 
 HARVEST_PERIOD=360
 if [ "`find /var/www/local/harvest/alma -maxdepth 1 -mmin -$HARVEST_PERIOD -name last_harvest.txt`" == "" ] ; then
-    /var/www/vufind/harvest/harvest_oai.sh
     touch /var/www/local/harvest/alma/last_harvest.txt
+    /var/www/vufind/harvest/harvest_oai.sh
 fi
 
 HARVEST_PERIOD=1440
 if [ "`find /var/www/local/harvest -maxdepth 1 -mmin -$HARVEST_PERIOD -name harvest_degruyter.json`" == "" ] ; then
-    /var/www/vufind/harvest/harvest_degruyter.sh
     touch /var/www/local/harvest/harvest_degruyter.json
+    /var/www/vufind/harvest/harvest_degruyter.sh
 fi
 
 exit $RET
