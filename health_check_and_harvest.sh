e8a9f61d39ffa29487f91d2c4d1b422a392500e9
Mateusz Żółtak <zozlak@zozlak.org>
2022-06-02 15:17:27 +0200

    health_check_and_harvest.sh: simplify the test

--- health_check_and_harvest.sh
+++ health_check_and_harvest.sh
@@ -3,13 +3,13 @@ curl http://127.0.0.1/vufind/ > /dev/null 2>&1
 RET=$?
 
 HARVEST_PERIOD=360
-if [ "`find /var/www/local/harvest/alma -maxdepth 1 -mmin -$HARVEST_PERIOD -name last_harvest.txt`" == "" ] ; then
+if [ "`find /var/www/local/harvest/alma/last_harvest.tx -maxdepth 0 -mmin -$HARVEST_PERIOD`" == "" ] ; then
     touch /var/www/local/harvest/alma/last_harvest.txt
     /var/www/vufind/harvest/harvest_oai.sh
 fi
 
 HARVEST_PERIOD=1440
-if [ "`find /var/www/local/harvest -maxdepth 1 -mmin -$HARVEST_PERIOD -name harvest_degruyter.json`" == "" ] ; then
+if [ "`find /var/www/local/harvest/harvest_degruyter.json -maxdepth 0 -mmin -$HARVEST_PERIOD`" == "" ] ; then
     touch /var/www/local/harvest/harvest_degruyter.json
     /var/www/vufind/harvest/harvest_degruyter.sh /var/www/local/harvest/harvest_degruyter.json $DEGRUYTER_PSWD
 fi
