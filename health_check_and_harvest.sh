5042f965d807d662dc887fe851254b6307311a8a
Mateusz Żółtak <zozlak@zozlak.org>
2022-06-03 08:33:11 +0200

    health_check_and_harvest.sh: allow enforcing delay with a parameter

--- health_check_and_harvest.sh
+++ health_check_and_harvest.sh
@@ -4,14 +4,16 @@ RET=$?
 
 SOLR_USER=${SOLR_USER:=8983}
 
-HARVEST_PERIOD=360
+HARVEST_PERIOD=$1
+HARVEST_PERIOD={$HARVEST_PERIO:=360}
 chown $SOLR_USER /var/www/local/harvest/alma
 if [ "`find /var/www/local/harvest/alma -maxdepth 1 -name last_harvest.txt -mmin -$HARVEST_PERIOD`" == "" ] ; then
     touch /var/www/local/harvest/alma/last_harvest.txt
     nohup /var/www/vufind/harvest/harvest_oai.sh >> /var/www/vufind/harvest/harvest_oai.log 2>&1
 fi
 
-HARVEST_PERIOD=1440
+HARVEST_PERIOD=$1
+HARVEST_PERIOD={$HARVEST_PERIO:=1440}
 chown $SOLR_USER /var/www/local/harvest/degruyter
 if [ "`find /var/www/local/harvest -maxdepth 1 -name harvest_degruyter.json -mmin -$HARVEST_PERIOD`" == "" ] ; then
     touch /var/www/local/harvest/harvest_degruyter.json
