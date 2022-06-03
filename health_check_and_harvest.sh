644b16d4849a6109edaf60eabb0b91a851005a49
Mateusz Żółtak <zozlak@zozlak.org>
2022-06-03 08:43:16 +0200

    health_check_and_harvest.sh: fixed HARVEST_PERIOD value setting

--- health_check_and_harvest.sh
+++ health_check_and_harvest.sh
@@ -4,16 +4,22 @@ RET=$?
 
 SOLR_USER=${SOLR_USER:=8983}
 
-HARVEST_PERIOD=$1
-HARVEST_PERIOD={$HARVEST_PERIO:=360}
+if [ "$1" != "" ] ; then
+    HARVEST_PERIOD=$1
+else
+    HARVEST_PERIOD=360
+fi
 chown $SOLR_USER /var/www/local/harvest/alma
 if [ "`find /var/www/local/harvest/alma -maxdepth 1 -name last_harvest.txt -mmin -$HARVEST_PERIOD`" == "" ] ; then
     touch /var/www/local/harvest/alma/last_harvest.txt
     nohup /var/www/vufind/harvest/harvest_oai.sh >> /var/www/vufind/harvest/harvest_oai.log 2>&1
 fi
 
-HARVEST_PERIOD=$1
-HARVEST_PERIOD={$HARVEST_PERIO:=1440}
+if [ "$1" != "" ] ; then
+    HARVEST_PERIOD=$1
+else
+    HARVEST_PERIOD=1440
+fi
 chown $SOLR_USER /var/www/local/harvest/degruyter
 if [ "`find /var/www/local/harvest -maxdepth 1 -name harvest_degruyter.json -mmin -$HARVEST_PERIOD`" == "" ] ; then
     touch /var/www/local/harvest/harvest_degruyter.json
